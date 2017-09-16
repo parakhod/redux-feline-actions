81b5b605b5c53b44838003f22c034449d3dcf434
Andrey Korinskiy <parakhod@ParaMac.local>
2017-09-16 14:33:24 +0200

    readme added, logic changed

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-Redux feline actions
+# Redux feline actions
+Easiest way to create Redux actions
+## Installation
+__npm install -s redux-feline-actions__
+## Usage
+```
+import { createActions } from 'redux-feline-actions';
+
+const actions = createActions({
+  justSomeString: 'cats are cool', // will be called reducer JUST_SOME_STRING
+  someFunction: v => 'payload with ' + v,
+  someObject: {
+    use: 'cat',
+    method: 'eatFood',
+    meta: 'blackCatMetaValue', // meta: {blackCatMetaValue: true}
+    id: 'my_cat_number_one'
+  },
+  someObjectWithCustomPayload: {
+    payload: 'two cats and five kittens'
+    meta: {
+      one: 1,
+      two: 2
+    }
+  },
+  someMoreComplexFunction: (a, b, meta) => ({
+    payload: {
+      cats: a,
+      catFood: b
+    },
+    meta
+  }),
+  andIfINeedToCallSomeOtherReducer: {
+    useReducer: 'customReducer', // will be called reducer CUSTOM_REDUCER
+    payload: 'meow'
+  }
+})
+```
--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,9 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -15,28 +15,29 @@ var getReducerName = function getReducerName(s) {
 };
 
 var createActions = function createActions(actions) {
-  return Object.keys(actions).reduce(function (p, name) {
+  return (typeof actions === 'undefined' ? 'undefined' : _typeof(actions)) !== 'object' ? new Error('Cannot create actions from ' + (typeof actions === 'undefined' ? 'undefined' : _typeof(actions)) + ', should be an object') : Object.keys(actions).reduce(function (p, name) {
     return _extends({}, p, _defineProperty({}, name, function () {
       var action = actions[name];
 
-      var payloadData = typeof action === 'function' ? action.apply(undefined, arguments) : typeof action === 'string' || action === true ? arguments.length <= 0 ? undefined : arguments[0] : action;
+      var payloadData = typeof action === 'function' ? action.apply(undefined, arguments) : action;
 
-      var reducerName = getReducerName(typeof action === 'string' ? action : payloadData.useReducer || name);
+      var reducerName = getReducerName(payloadData.useReducer || name);
 
       var payload = {};
       var meta = {};
 
       if (payloadData) {
         payload = {
-          payload: payloadData
+          payload: payloadData.payload || payloadData
         };
 
         if (payloadData.meta) {
           meta = {
-            meta: _typeof(payloadData.meta) === 'object' ? payloadData.meta : {
+            meta: _typeof(payloadData.meta) === 'object' ? payloadData.meta : typeof payloadData.meta === 'string' ? _defineProperty({}, payloadData.meta, true) : {
               value: payloadData.meta
             }
           };
+          delete payload.payload.meta;
         }
       }
 
